e3e5427879a245f923d52f935b37a7eea8d8248d
Chenghao Wang <chenghao@unity3d.com>
2017-03-01 17:07:21 -0800

    update

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -22,27 +22,6 @@ public static class BuildHelper {
 	public static void SwitchPlatformsSamsungApps(UnityEngine.CloudBuild.BuildManifestObject manifest) {
 		UnityEngine.Purchasing.UnityPurchasingEditor.TargetAndroidStore(UnityEngine.Purchasing.AndroidStore.SamsungApps);
 	}
-
-	private static void Helper() {
-		using (var input = File.OpenText("Assets/Plugins/UnityPurchasing/script/IAPButton.cs"))
-		using (var output = new StreamWriter("Assets/Plugins/UnityPurchasing/script/IAPButton2.cs")) {
-			string line;
-			while (null != (line = input.ReadLine())) {
-				if (line.Contains("module.useFakeStoreUIMode = FakeStoreUIMode.StandardUser;")) {
-					output.WriteLine("#if UNITY_IOS && !UNITY_EDITOR");
-					output.WriteLine("module.useFakeStoreAlways = false;");
-					output.WriteLine("#else");
-					output.WriteLine("module.useFakeStoreAlways = true;");
-					output.WriteLine("#endif");
-				} else {
-					output.WriteLine(line);
-				}
-			}
-		}
-
-		File.Delete ("Assets/Plugins/UnityPurchasing/script/IAPButton.cs");
-		System.IO.File.Move("Assets/Plugins/UnityPurchasing/script/IAPButton2.cs", "Assets/Plugins/UnityPurchasing/script/IAPButton.cs");
-	}
 }
 
 
